8a55f5208a5e3975eafc369697dd961dfb06d922
Christian Juth <cjuth2@gmail.com>
2015-03-15 15:33:42 -0400

    extjs-install script update

--- ext-install.js
+++ ext-install.js
@@ -1,27 +1,32 @@
+//initilize Parse and import dependencies
 var Parse = require('parse').Parse;
 Parse.initialize("kZS90MpRoWUMtFEgdt8kdWSejhl3thUb9GPdekpb", "ND73SsKVmvrxJ04LmLNgN2lqulJqdjqfRkvI6n23");
 var exec = require('child_process').exec
 
-search = process.argv.splice(2)[0]
-path = 'app/ext-plugins/'
-
+//vars
+var search = process.argv.splice(2)[0]
+var path = 'app/ext-plugins/'
 var Plugin = Parse.Object.extend("Plugin");
+
+//query database
 var query = new Parse.Query(Plugin);
 query.equalTo("search", search.toLowerCase());
-query.notEqualTo("name","")
 query.notEqualTo("file", null)
 
+//results
 query.first({
   success: function(plug) {
 
+    //check if plugin exists
     if(typeof plug === "undefined"){
       console.log("Plugin not found.")
       return;
     }
 
+    //download plugin
     url = plug.get('file').url()
     exec('curl -o '+path+search+'.js '+url)
-
+    console.log(path+search+'.js')
     return;
 
   }
